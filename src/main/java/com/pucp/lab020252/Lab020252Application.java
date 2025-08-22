eca9701caf190e2fbbee459eb7102c26e71bcd96
jasnai10 <a20223806@pucp.edu.pe>
2025-08-22 00:28:48 -0500

    feat: imprimiendo lista

--- src/main/java/com/pucp/lab020252/Lab020252Application.java
+++ src/main/java/com/pucp/lab020252/Lab020252Application.java
@@ -3,6 +3,8 @@ package com.pucp.lab020252;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Lab020252Application {
 
@@ -10,6 +12,10 @@ public class Lab020252Application {
 
 		SpringApplication.run(Lab020252Application.class, args);
 		System.out.println("Hola Mundo!");
+		List<String> lista = List.of("Elemento1", "Elemento2", "Elemento3");
+		for (String s : lista) {
+			System.out.println("elemento: " + s);
+		}
 	}
 
 }
