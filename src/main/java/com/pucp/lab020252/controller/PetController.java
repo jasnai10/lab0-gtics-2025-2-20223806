2fd4a01532fbd40ee67203acb522b301af8dda0e
jasnai10 <a20223806@pucp.edu.pe>
2025-08-22 01:21:58 -0500

    feat: log

--- src/main/java/com/pucp/lab020252/controller/PetController.java
+++ src/main/java/com/pucp/lab020252/controller/PetController.java
@@ -1,12 +1,16 @@
 package com.pucp.lab020252.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class PetController {
     @GetMapping("/pets")
-    public String getPets() {
+    public String getPtets() {
+        log.info("Pet controller accessed");
         return "pets";
     }
+
 }
